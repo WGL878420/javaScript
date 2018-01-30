0a41c19756d39927090bccd598258c85124d62bc
Wxh16144 <wxh16144@qq.com>
2018-01-30 17:54:01 +0800

    Revert "Revert "Revert "鼠标移动效果"""
    This reverts commit b42fc12a721a040185369e8608e2f28a33cc6bf2.

--- 轮播图/鼠标移动效果/index.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-    $('#box ol li').mouseover(function(event){
-        var index=$(this).index(); //获取当前的索引号
-        $('#box ul li').eq(index).fadeIn().siblings().hide();
-        $(this).addClass('courrent').siblings().removeClass('courrent');
-        });
-});
